a2278ab86100d2235e6f3d434cbdd0e9f052aa9d
PaarkG <2007kdogrules@gmail.com>
2026-01-16 23:54:42 -0500

    implemented mealplans as an enum, added advance week to player

--- src/player.js
+++ src/player.js
@@ -1,7 +1,7 @@
 import { Stat } from "./stat.js";
 
 class Player {
-    constructor() {
+    constructor(mealplan) {
         this.grades = [];
 
         this.food = new Stat(initialValue = 100, hourlyChange = -5);
@@ -12,11 +12,15 @@ class Player {
         this.sanity = new Stat(initialValue = 100, hourlyChange = -1);
 
         this.stats = [this.food, this.water, this.energy, this.rizz, this.hygiene, this.sanity];
+
+        this.mealplan = mealplan;
     }
 
     advanceHour = () => this.stats.forEach((stat) => stat.advanceHour);
 
     advanceDay = () => this.stats.forEach((stat) => stat.advanceDay);
+
+    advanceWeek = () => this.mealplan.resetSwipes();
 
     advanceSleep = () => this.stats.forEach((stat) => stat.advanceSleep);
 }
